ea1ed14ed7e4735ebe18c487fabfa3b769e9460d
Arlan Askapov <turganbekoaamangeldy@gmail.com>
2025-04-03 23:06:36 +0500

    hm23

--- routes/courseRoutes.js
+++ routes/courseRoutes.js
@@ -1,12 +1,29 @@
 import express from "express";
-import { createCourse, getCourses, getCourse, updateCourse, deleteCourse } from "../controllers/courseController.js";
+import { body } from "express-validator";
+import { validate } from "../middleware/validate.js";
+import {
+  createCourse,
+  getCourses,
+  getCourse,
+  updateCourse,
+  deleteCourse,
+} from "../controllers/courseController.js";
 
 const router = express.Router();
 
-router.post("/", createCourse);
+const courseValidationRules = [
+  body("title").notEmpty().withMessage("Название курса обязательно"),
+  body("description")
+    .optional()
+    .isLength({ min: 10 })
+    .withMessage("Описание должно быть минимум 10 символов"),
+  body("author").notEmpty().withMessage("Автор обязателен"),
+];
+
+router.post("/", validate(courseValidationRules), createCourse);
 router.get("/", getCourses);
 router.get("/:id", getCourse);
-router.patch("/:id", updateCourse);
+router.patch("/:id", validate(courseValidationRules), updateCourse);
 router.delete("/:id", deleteCourse);
 
 export default router;
